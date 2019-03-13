72b0d693e87b1bb7b42f89f9e8fc032f5aecc040
j4sysiak <j4sysiak@gmail.com>
2019-03-13 19:42:13 +0100

    1

---
+++ spring-boot-tutorial-Email-Verification-089b/src/main/java/com/jaceksysiak/service/EmailService.java
@@ -0,0 +1,71 @@
+package com.jaceksysiak.service;
+
+import java.util.Date;
+
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+//import org.thymeleaf.TemplateEngine;
+//import org.thymeleaf.context.Context;
+//import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Value("${mail.enable}")
+	private Boolean enable;
+	
+	private void send(MimeMessagePreparator preparator){
+		
+		if(enable){
+			mailSender.send(preparator);
+		}
+	}
+	
+	public void sendVerificationEmail(String emailAddress){
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("<HTML>");
+		sb.append("<p>Hello there, this is <strong>HTML</strong></p>");
+		sb.append("</HTML>");
+		
+//		Context context = new Context();
+//		
+//		context.setVariable("name", "Jacek");
+//		
+//		final String emailContents = templateEngine.process("verifyemail", context);
+//		
+//		System.out.println(emailContents);
+		
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				 
+				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				
+				message.setTo(emailAddress);
+				message.setFrom(new InternetAddress("jaceksysiak@wp.pl"));
+				message.setSubject("Please Verify Your Email Address");
+				message.setSentDate(new Date());
+				
+				message.setText(sb.toString(), true);
+				 
+			}
+		};
+		
+		send(preparator);
+		
+	}
+
+}
